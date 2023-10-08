66f9b5283c006a851e30bff65bd81f4a889f2ec1
BryanMgbeojirikwe <73403625+BryanMgbeojirikwe@users.noreply.github.com>
2023-10-08 17:24:19 -0400

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,28 @@
+import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import LandingPage from "./Pages/LandingPage";
 import MainSection from "./sections/MainSection";
 import SideSection from "./sections/SideSection";
+import SignUp from "./Pages/SignUp";
+import LoginPage from "./Pages/LoginPage";
 
 function App() {
-    return (
-            <div className="row">
-                <div className="col-lg-8 nopadding">
-                    <MainSection />
-                </div>
-                <div className="col-lg-4 nopadding">
-                    <SideSection />
-                </div>
-            </div>
-    );
+  return (
+    <Router>
+      <Routes>
+        {/* Default route for the landing page */}
+        <Route path="/" element={<LandingPage />} />
+
+        {/* Other routes */}
+        <Route path="/SignUp" element={<SignUp />} />
+        <Route path="/LoginPage" element={<LoginPage />} />
+        <Route path="/important" element={<MainSection />} />
+        <Route path="/side" element={<SideSection />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
+
