d48f8a04ca88cdaea3e1af84885fe082236c6935
seamus2002 <seamus.english58@myhunter.cuny.edu>
2023-10-09 20:37:57 -0400

    fix most css

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -3,10 +3,14 @@ import SideSection from "../sections/SideSection"
 
 const MainPage = () => {
   return (
-    <>
-      <MainSection />
-      <SideSection />
-    </>
+    <div className="row">
+      <div className="col-lg-8 nopadding">
+        <MainSection />
+      </div>
+      <div className="col-lg-4 nopadding">
+        <SideSection />
+      </div>
+    </div>
   )
 }
 
