108262e3a9af8006b4f9dcf88e6d77ded76ec6cc
seamus2002 <seamus.english58@myhunter.cuny.edu>
2023-10-11 20:42:01 -0400

    add icon library

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,20 @@
 const Header = () => {
   return (
-    <div className="header">Header</div>
+    <div className="header">
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-1">
+            <i class="bi bi-three-dots-vertical"></i>
+          </div>
+          <div className="col-sm-10">
+            Date
+          </div>
+          <div className="col-sm-1">
+            <i className="bi bi-calendar"></i>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
