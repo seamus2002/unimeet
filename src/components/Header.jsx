12fe5d43b5ee8f4a3dc67772e413c54b90ee2593
seamus2002 <seamus.english58@myhunter.cuny.edu>
2023-10-11 20:55:58 -0400

    made header look nicer

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,9 +6,22 @@ const Header = () => {
           <div className="col-sm-1">
             <i class="bi bi-three-dots-vertical"></i>
           </div>
+
+          {/* Date Panel */}
           <div className="col-sm-10">
-            Date
+            <div className="row">
+              <div className="col-sm-1">
+                <i class="bi bi-caret-left-fill"></i>
+              </div>
+              <div className="col-sm-10">
+                Date
+              </div>
+              <div className="col-sm-1">
+                <i class="bi bi-caret-right-fill"></i>
+              </div>
+            </div>
           </div>
+          
           <div className="col-sm-1">
             <i className="bi bi-calendar"></i>
           </div>
