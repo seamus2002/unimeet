1406ffdcdfb02a93b47f32db58fc2ce71cd050b8
Seamus <theseamusenglish@gmail.com>
2023-10-25 18:23:18 -0400

    remove slice function on the nametag

--- src/components/Nametag.jsx
+++ src/components/Nametag.jsx
@@ -2,7 +2,8 @@ const Nametag = ({ displayName }) => {
   return (
     <div className="nametag-container">
       <div className="nametag">
-        {displayName.split(" ").slice(0, -1).join(" ")}
+        {displayName}
+        {/* {displayName.split(" ").slice(0, -1).join(" ")} */}
       </div>
     </div>
   );
