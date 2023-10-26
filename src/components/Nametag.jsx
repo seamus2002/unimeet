2f24e4d24594b23ca85795538a5d1e690d2310e2
Seamus <theseamusenglish@gmail.com>
2023-10-25 21:37:35 -0400

    fix spacing issue on nametag

--- src/components/Nametag.jsx
+++ src/components/Nametag.jsx
@@ -3,7 +3,7 @@ const Nametag = ({ displayName }) => {
     <div className="nametag-container">
       <div className="nametag">
         {
-          displayName.includes(" ")
+          /\s/.test(displayName)
             ? displayName.split(" ")[0].length > 8
               ? displayName.split(" ")[0].slice(0, 8) + "..." // Display the first 8 characters of the first name with an ellipsis
               : displayName.split(" ")[0] // Display the first name as is if it's 8 characters or shorter
