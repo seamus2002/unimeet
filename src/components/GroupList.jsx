6722cf88028b5dec7b0cb3c1edb7ea63ef7f4c57
Seamus <theseamusenglish@gmail.com>
2023-10-25 21:43:59 -0400

    fix missing info and put email as placeholder

--- src/components/GroupList.jsx
+++ src/components/GroupList.jsx
@@ -59,14 +59,16 @@ const GroupList = () => {
   return (
     <div className="group-list d-flex align-items-center justify-content-center">
       <UserIcon
-        photoURL={currentUser.photoURL}
-        displayName={currentUser.displayName}
+        photoURL={currentUser.photoURL ? currentUser.photoURL : test_user}
+        displayName={
+          currentUser.displayName ? currentUser.displayName : currentUser.email
+        }
       />
       {memberInfo.map((info, index) => (
         <UserIcon
           key={index}
           photoURL={info.photoURL ? info.photoURL : test_user}
-          displayName={info.displayName}
+          displayName={info.displayName ? info.displayName : info.email}
         />
       ))}
     </div>
