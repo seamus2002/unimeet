5fcbe4bf0659e5e4247154a4b01557c81dd7c654
BryanMgbeojirikwe <73403625+BryanMgbeojirikwe@users.noreply.github.com>
2023-11-11 20:20:14 -0500

    added back button to settings page cuz i forgot

--- src/pages/Settings/Settings.jsx
+++ src/pages/Settings/Settings.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
-import { Link } from "react-router-dom";
+import { useNavigate } from 'react-router-dom'; // Import useNavigate
 import "./Settings.css";
 
 const Settings = () => {
+  const navigate = useNavigate(); // Initialize useNavigate
+
   return (
     <div className="settings-container">
+      {/* Back button */}
+      <button className="back-button" onClick={() => navigate('/')}>
+        Back
+      </button>
+
       <h1>Welcome to the settings page</h1>
     </div>
   );
