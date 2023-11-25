7b1f63cad1e26b8cbe9cb5e7588c98d14fe23ca9
DavidA123777 <ddda4167@gmail.com>
2023-11-25 16:11:38 -0500

    updated the styling for Settings page

--- src/pages/Settings/Settings.jsx
+++ src/pages/Settings/Settings.jsx
@@ -1,5 +1,5 @@
 import { useContext, useState } from "react";
-import { useNavigate } from "react-router-dom"; // Import useNavigate
+import { useNavigate } from "react-router-dom";
 import "./Settings.css";
 import { UserContext } from "../../contexts/UserContext";
 import { updateUser } from "../../utils/firebase/firebase.utils";
@@ -7,8 +7,8 @@ import { updateUser } from "../../utils/firebase/firebase.utils";
 const Settings = () => {
   const navigate = useNavigate();
   const { currentUser } = useContext(UserContext);
-  const [displayName, setDisplayName] = useState(currentUser.displayName || "");
-  const [photoURL, setPhotoURL] = useState(currentUser.photoURL || "");
+  const [displayName, setDisplayName] = useState(currentUser?.displayName || "");
+  const [photoURL, setPhotoURL] = useState(currentUser?.photoURL || "");
 
   const handleDisplayNameChange = (e) => {
     setDisplayName(e.target.value);
@@ -25,29 +25,41 @@ const Settings = () => {
         Back
       </button>
 
-      <div>
-        <h2>User Settings</h2>
-        <div>
-          <label htmlFor="displayName">Display Name:</label>
-          <input
-            type="text"
-            id="displayName"
-            value={displayName}
-            onChange={handleDisplayNameChange}
-          />
+      <div className="settings-content">
+        <h1>User Settings</h1>
+        <div className="squiggly-line">
+          <svg width="200" height="10" xmlns="http://www.w3.org/2000/svg">
+            <path
+              d="M0,5 Q20,0 40,5 Q60,10 80,5 Q100,0 120,5 Q140,10 160,5 Q180,0 200,5"
+              fill="none"
+              stroke="black"
+              strokeWidth="2"
+            />
+          </svg>
         </div>
-        <div>
-          <label htmlFor="photoURL">Photo URL:</label>
-          <input
-            type="text"
-            id="photoURL"
-            value={photoURL}
-            onChange={handlePhotoURLChange}
-          />
+        <div className="input-group">
+          <div className="input-field one">
+            <label htmlFor="displayName">Display Name:</label>
+            <input
+              type="text"
+              id="displayName"
+              value={displayName}
+              onChange={handleDisplayNameChange}
+            />
+          </div>
+          <div className="input-field">
+            <label htmlFor="photoURL">Photo URL:</label>
+            <input
+              type="text"
+              id="photoURL"
+              value={photoURL}
+              onChange={handlePhotoURLChange}
+            />
+          </div>
+          <button className="saveBtn" onClick={updateUser}>
+            Save Changes
+          </button>
         </div>
-        <button className="btn btn-primary" onClick={updateUser}>
-          Save Changes
-        </button>
       </div>
     </div>
   );
