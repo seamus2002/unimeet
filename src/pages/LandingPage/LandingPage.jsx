bf3ff59b52fa5a53339ff3e193c41ba3356057ae
DavidA123777 <ddda4167@gmail.com>
2023-12-03 20:35:59 -0500

    edited landing page to have cards of key value propositions

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -36,36 +36,36 @@ const Landing = () => {
         <h2 className="section-title">Why Choose UNIMEET?</h2>
       <div className="card-container">
         <div className="phrase-container">
-          <p className="phrase1">
-            Revolutionize the way you plan meetups and connect with friends.
-          </p>
           <img
             src="https://th.bing.com/th/id/OIP.geOxNyQMHIGgZPP0ZMKr8wHaFS?pid=ImgDet&rs=1"
             alt="Revolutionize Image"
             className="phrase-image"
           />
+          <p className="phrase1">
+            Revolutionize the way you plan meetups and connect with friends.
+          </p>
         </div>
         <div className="phrase-container">
-          <p className="phrase">
-            Say goodbye to scheduling conflicts and hello to hassle-free
-            meetups.
-          </p>
           <img
             src="https://assets.website-files.com/634681057b887c6f4830fae2/6367ddcb71d5e9741bb2de9c_6259f7e1d6bb00e39f6ac2e8_schedule_conflict.png"
             alt="Scheduling Conflicts Image"
             className="phrase-image"
           />
+          <p className="phrase">
+            Say goodbye to scheduling conflicts and hello to hassle-free
+            meetups.
+          </p>
         </div>
 
         <div className="phrase-container">
-          <p className="phrase3">
-            Experience the future of meetup coordination with UNIMEET.
-          </p>
-          <img
+        <img
             src="https://thumbs.dreamstime.com/b/people-meeting-together-outdoor-friends-gathering-vector-illustration-concept-friend-meetup-celebration-collab-collaboration-197968976.jpg"
             alt="Future of Meetup Coordination Image"
             className="phrase-image"
           />
+          <p className="phrase3">
+            Experience the future of meetup coordination with UNIMEET.
+          </p>
         </div>
       </div>
       </section>
