a2c8957a382f27ee32e7bdd61887d53dccde2208
KyleAnthonyHay <haykyle917@gmail.com>
2023-12-07 13:11:23 -0500

    Events context started

--- src/components/OptimalEventTime/FindOptimalTime.jsx
+++ src/components/OptimalEventTime/FindOptimalTime.jsx
@@ -1,2 +1,11 @@
 import React, { useContext} from "react";
 import { useEvents } from "../../contexts/EventsContext";
+
+export default function FindOptimalTime() {
+    const events = useContext(useEvents);
+
+
+    return (
+        <div> </div>
+    )
+}
