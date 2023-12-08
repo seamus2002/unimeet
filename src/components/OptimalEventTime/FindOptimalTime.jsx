9da3d5df1590c9a4ab58f53e4026177ce1066ebe
Seamus <theseamusenglish@gmail.com>
2023-12-08 10:36:21 -0500

    fix event.start in findoptimaltime

--- src/components/OptimalEventTime/FindOptimalTime.jsx
+++ src/components/OptimalEventTime/FindOptimalTime.jsx
@@ -1,15 +1,15 @@
-import React, { useContext} from "react";
 import { useEvents } from "../../contexts/EventsContext";
 
 export default function FindOptimalTime() {
-    const { events } = useEvents();
-    
+  const { events } = useEvents();
 
-    
-
-    return (
-        <div>{events.map(event => <p>{event.start
-            ? new Date(event.start.seconds * 1000).toLocaleTimeString()
-            : "N/A"}</p>)} </div>
-    )
+  return (
+    <div>
+      {events.map((event) => (
+        <p>
+          {event.start ? new Date(event.start).toLocaleTimeString() : "N/A"}
+        </p>
+      ))}{" "}
+    </div>
+  );
 }
