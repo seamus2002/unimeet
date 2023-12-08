37191117ae5292d90aa9072289cbee0d66bf95d9
KyleAnthonyHay <haykyle917@gmail.com>
2023-12-08 10:41:40 -0500

    Times Recoeved

--- src/components/OptimalEventTime/FindOptimalTime.jsx
+++ src/components/OptimalEventTime/FindOptimalTime.jsx
@@ -3,13 +3,24 @@ import { useEvents } from "../../contexts/EventsContext";
 export default function FindOptimalTime() {
   const { events } = useEvents();
 
+  const startTimes = events.map((event) => (
+      event.start ? new Date(event.start).toLocaleTimeString() : "N/A"
+  ))
+
+  const endTimes = events.map((event) => (
+    event.start ? new Date(event.end).toLocaleTimeString() : "N/A"
+))
+
   return (
     <div>
-      {events.map((event) => (
-        <p>
-          {event.start ? new Date(event.start).toLocaleTimeString() : "N/A"}
-        </p>
-      ))}{" "}
+      {endTimes}
     </div>
   );
 }
+
+
+  {/* {events.map((event) => (
+        <p>
+          {event.start ? new Date(event.start).toLocaleTimeString() : "N/A"}
+        </p>
+      ))}{" "} */}
