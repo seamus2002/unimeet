9b11ccab7ad41b168e098b77ce65a61ba67d37b0
KyleAnthonyHay <haykyle917@gmail.com>
2023-12-09 16:17:15 -0500

    date context added

--- src/components/OptimalEventTime/FindOptimalTime.jsx
+++ src/components/OptimalEventTime/FindOptimalTime.jsx
@@ -1,19 +1,25 @@
+import { DateContext } from "../../contexts/DateContext";
 import { useEvents } from "../../contexts/EventsContext";
 
 export default function FindOptimalTime() {
   const { events } = useEvents();
+  const { currentDate } = useContext(DateContext);
 
   const startTimes = events.map((event) => (
-      event.start ? new Date(event.start).toLocaleTimeString() : "N/A"
+      event.start ? new Date(event.start).toString() : "N/A"
   ))
 
   const endTimes = events.map((event) => (
-    event.start ? new Date(event.end).toLocaleTimeString() : "N/A"
+    event.start ? new Date(event.end).toString() : "N/A"
 ))
 
   return (
     <div>
-      {endTimes}
+      {events.map((event) => (
+        <p>
+          {event.start ? new Date(event.start).toString() : "N/A"}
+        </p>
+      ))}{" "}
     </div>
   );
 }
