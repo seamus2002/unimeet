5267319a52a26bc6ac61811b7663596292bb1cf9
Seamus <theseamusenglish@gmail.com>
2023-12-17 21:05:18 -0500

    add back button to groups page

--- src/pages/GroupsPage/GroupsPage.jsx
+++ src/pages/GroupsPage/GroupsPage.jsx
@@ -1,12 +1,19 @@
 import { useContext } from "react";
 import AddUserToGroup from "../../components/AddUserToGroup/AddUserToGroup";
 import { GroupContext } from "../../contexts/GroupContext";
+import { useNavigate } from "react-router-dom";
 
 const GroupsPage = () => {
   const { currentGroup } = useContext(GroupContext);
+  const navigate = useNavigate();
 
   return (
     <div>
+      {/* Back button */}
+      <button className="back-button" onClick={() => navigate("/")}>
+        Back
+      </button>
+
       {!currentGroup ? (
         <h1>No Group Found</h1>
       ) : (
