b5dae25df58bb701db5b720949b36fb896868241
Seamus <theseamusenglish@gmail.com>
2023-12-19 22:36:50 -0500

    update package.json and vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://seamus2002.github.io/unimeet",
   plugins: [react()],
 });
